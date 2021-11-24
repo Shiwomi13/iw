3aae58c1a063bd07456dfc6536b73a2e2b8049dd
Shiwomi13 <yaroslavaleksandrov11@mail.ru>
2021-11-24 21:45:48 +0300

    lab3

---
+++ labs/lab1/Aleksandrov/AleksandrovLog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aleksandrov
+{
+    class AleksandrovLog:LogAbstract, LogInterface
+    {
+        private static AleksandrovLog instance;
+        List<string> ListArr = new List<string>();
+        public static AleksandrovLog I()
+        {
+            if (instance == null)
+                instance = new AleksandrovLog();
+            return instance;
+        }
+        public LogInterface Log(string str)
+        {
+            ListArr.Add(str);
+            return this;
+        }
+        public LogInterface Write()
+        {
+            writeConsole(ListArr.ToArray());
+            return this;
+        }
+    }
+}
---
+++ labs/lab1/Aleksandrov/Kvad.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aleksandrov
+{
+    class Kvad : Lin, EquationInterface
+    {
+        protected float Discriminant(float a, float b, float c)
+        {
+            return b * b - (4 * a * c);
+        }
+        public List<float> Solve(float a, float b, float c)
+        {
+            if (a == 0)
+                return Check(b, c);
+            AleksandrovLog.I().Log("Определено, что это квадратное уравнение");
+            float disc = Discriminant(a, b, c);
+            if (disc < 0)
+                throw new AleksandrovException("Ошибка: уравнение не имеет решений");
+            if (disc == 0)
+                return x = new List<float>() { -b / (2 * a) };
+            disc = (float)Math.Sqrt(disc);
+            return x = new List<float> {
+                (-b + disc) / (2 * a),
+                (-b - disc) / (2 * a)
+            };
+        }
+    }
+}
