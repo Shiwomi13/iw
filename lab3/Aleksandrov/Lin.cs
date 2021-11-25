3dba9f00a1916133503ceaefe948042688556466
Shiwomi13 <yaroslavaleksandrov11@mail.ru>
2021-11-25 23:02:45 +0300

    lab3

---
+++ lab3/Aleksandrov/Lin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aleksandrov
+{
+    class Lin
+    {
+        public List<float> x;
+        public List<float> Check(float b, float c)
+        {
+            if (b == 0)
+                throw new AleksandrovException("Определено, что такое уравнение не существует"); 
+            AleksandrovLog.I().Log("Определено, что это линейное уравнение");
+            return x = new List<float>() { -c / b };
+        }
+    }
+}
---
+++ lab3/Core/LogInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aleksandrov
+{
+    interface LogInterface
+    {
+        LogInterface Log(string str);
+        LogInterface Write();
+    }
+}
